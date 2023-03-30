2b76fcf1a4d63ddd15be8c31eb38ed2f3f3fa591
j0nis <jonathan.nissinen@hotmail.com>
2023-03-30 13:01:23 +0300

    Muutettu niin että voi kirjautua sisään tuotteet sivulta

--- VerkkokauppaWeb/ViewModels/TuotteetAndLoginsViewModel.cs
+++ VerkkokauppaWeb/ViewModels/TuotteetAndLoginsViewModel.cs
@@ -11,16 +11,7 @@ namespace VerkkokauppaWeb.ViewModels
     {
         public IEnumerable<Tuotteet> Tuotteet { get; set; }
         public Logins Logins { get; set; }
-        
-        public int TuoteID { get; set; }
-        public int KategoriaID { get; set; }
-        public string Nimi { get; set; }
-        public decimal Hinta { get; set; }
-        public int Varastomaara { get; set; }
-        public string Kuvaus { get; set; }
-        public string KuvaPolku { get; set; }
-        public byte[] Kuva { get; set; }
-        public virtual Kategoriat Kategoriat { get; set; }
+
         public int LoginID { get; set; }
         public int AsiakasID { get; set; }
         public string Email { get; set; }
@@ -29,6 +20,5 @@ namespace VerkkokauppaWeb.ViewModels
         public string LoginErrorMessage { get; internal set; }
 
         public virtual Asiakkaat Asiakkaat { get; set; }
-
     }
 }
