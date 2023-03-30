2e9236e39383cceb92944e1ec0588ae2b6845ab9
j0nis <jonathan.nissinen@hotmail.com>
2023-03-30 11:39:28 +0300

    tein ViewModel kansion jossa tiedosto

---
+++ VerkkokauppaWeb/ViewModels/TuotteetAndLoginsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VerkkokauppaWeb.Models;
+
+namespace VerkkokauppaWeb.ViewModels
+{
+    public class TuotteetAndLoginsViewModel
+    {
+        
+        public int TuoteID { get; set; }
+        public int KategoriaID { get; set; }
+        public string Nimi { get; set; }
+        public decimal Hinta { get; set; }
+        public int Varastomaara { get; set; }
+        public string Kuvaus { get; set; }
+        public string KuvaPolku { get; set; }
+        public byte[] Kuva { get; set; }
+        public virtual Kategoriat Kategoriat { get; set; }
+        public int LoginID { get; set; }
+        public int AsiakasID { get; set; }
+        public string Email { get; set; }
+        public string Salasana { get; set; }
+
+        public string LoginErrorMessage { get; internal set; }
+
+        public virtual Asiakkaat Asiakkaat { get; set; }
+
+    }
+}
